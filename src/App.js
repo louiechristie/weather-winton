9d166808a766cf0145f796aa47e452e1102e430a
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2019-02-21 15:12:42 +0000

    Setup eslint and prettier

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ class App extends Component {
     return (
       <div className="app">
         <header className="header">
-          <h1>
-            Weather Winton
-          </h1>
+          <h1>Weather Winton</h1>
           <p>A weather widget.</p>
         </header>
       </div>
