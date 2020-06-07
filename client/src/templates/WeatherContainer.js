91632c95827ae8aab14bf4a52ba85e297ff38128
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-06-07 03:19:30 +0100

    Add 'today' to title

--- client/src/templates/WeatherContainer.js
+++ client/src/templates/WeatherContainer.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 import SimpleAppBar from '../components/SimpleAppBar';
 import WeatherWidget from '../components/WeatherWidget';
 import Footer from '../components/Footer';
 
 export default ({ pageContext: { items, meta } }) => {
-  const todaysWeather =
-    (items && items[0] && items[0].description) || 'probably raining';
-
+  const todaysWeather = `Today: ${
+    (items && items[0] && items[0].description) || 'probably raining'
+  }`;
+  // const date =  `${dayjs().format('D MMM YY')}`
   return (
     <>
       <SimpleAppBar
