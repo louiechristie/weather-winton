5b11e03e466d22ade061a5d63b648a3ec6e05b32
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-06-07 01:41:03 +0100

    Update client dependencies

--- client/src/templates/WeatherContainer.js
+++ client/src/templates/WeatherContainer.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
-import ErrorBoundary from 'react-error-boundary';
-
 import SimpleAppBar from '../components/SimpleAppBar';
 import WeatherWidget from '../components/WeatherWidget';
 import Footer from '../components/Footer';
@@ -12,7 +9,7 @@ export default ({ pageContext: { items, meta } }) => {
     (items && items[0] && items[0].description) || 'probably raining';
 
   return (
-    <ErrorBoundary>
+    <>
       <SimpleAppBar
         title={`${todaysWeather}`}
         description={`${meta.title}`}
@@ -24,6 +21,6 @@ export default ({ pageContext: { items, meta } }) => {
       <WeatherWidget items={items} />
 
       <Footer meta={meta} />
-    </ErrorBoundary>
+    </>
   );
 };
