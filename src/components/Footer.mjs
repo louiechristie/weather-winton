40668e9ecf6199ef5585da448fa1837f0a23167c
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2024-05-23 11:34:17 +0100

    Hosted on badge depending on Vercel or Netlify

--- src/components/Footer.mjs
+++ src/components/Footer.mjs
@@ -18,19 +18,51 @@ const styles = {
   },
 };
 
-const Footer = (props) => {
+const HostedOn = () => {
+  if (process.env.NETLIFY) {
+    return (
+      <>
+        <img
+          style={{ verticalAlign: 'middle' }}
+          src="https://api.netlify.com/api/v1/badges/f0ee41e4-eb3b-44f3-8810-07d0a72ca9a1/deploy-status"
+          alt="Netlify logo"
+        />
+      </>
+    );
+  }
+
+  if (process.env.VERCEL) {
+    return (
+      <>
+        <img
+          style={{ verticalAlign: 'middle' }}
+          src="https://deploy-badge.vercel.app/vercel/weather-winton"
+          alt="Vercel logo"
+        />
+      </>
+    );
+  }
+
+  return <>Local</>;
+};
+
+const Footer = props => {
   const { meta } = props;
   return (
     <Paper style={styles.footer} elevation={24} square>
       <Typography variant="body2" component="p" align="center">
-        Last updated: {meta.timeStamp} | Weather Winton ©️ {dayjs().year()} v
-        {meta.version} | Tags: user experience, UX | Hosted on: <img style={{verticalAlign: 'middle'}} src="https://api.netlify.com/api/v1/badges/f0ee41e4-eb3b-44f3-8810-07d0a72ca9a1/deploy-status" alt="Netlify logo" />
+        Last updated: {meta.timeStamp} | Hosted on: {HostedOn()}
+      </Typography>
+
+      <Typography variant="body2" component="p" align="center">
+        Weather Winton ©️ {dayjs().year()} v{meta.version} | Tags: user
+        experience, UX
       </Typography>
 
       <Typography variant="body2" component="p" paragraph align="center">
-        Data from: Met Office | Contains public sector information licensed under the Open Government Licence
+        Data from: Met Office | Contains public sector information licensed
+        under the Open Government Licence
       </Typography>
-
 
       <Typography
         variant="body2"
