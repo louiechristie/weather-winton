affad9d98af9450672c3fcc516ad78ad464222f7
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-03-20 16:26:42 +0000

    Remove redundant dependencies

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -4,15 +4,5 @@ dotenv.config();
 
 export default {
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-html-attributes',
-      options: {
-        lang: 'en',
-      },
-    },
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-twitter`,
   ],
 };
