4631de4b999690bb04e17db6a90232ebfd2aef87
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-06 09:08:43 +0000

    chore: update lintint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,15 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
 import neostandard from 'neostandard';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import typescript from 'eslint-config-next/typescript';
+import { defineConfig } from 'eslint/config';
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
+export default defineConfig([
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...nextVitals,
+  ...typescript,
   ...neostandard({
     noStyle: true, // Disable style-related rules (useful with Prettier or dprint)
     globals: globals.jest,
@@ -37,6 +28,4 @@ const eslintConfig = [
       ],
     },
   },
-];
-
-export default eslintConfig;
+]);
