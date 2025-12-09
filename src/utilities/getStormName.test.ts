e3cbab91a42889ecb9dcfbb7fa1121cc0f8a3586
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-09 17:54:28 +0000

    test: storm Bram, happy path on another day

--- src/utilities/getStormName.test.ts
+++ src/utilities/getStormName.test.ts
@@ -7,4 +7,10 @@ describe('get storm name', () => {
 
     expect(getStormName(instant)).toEqual('Bram');
   });
+
+  test('should return Bram, when date is 8 December', () => {
+    const instant = Temporal.Instant.from('2025-12-08T00:00:00Z');
+
+    expect(getStormName(instant)).toEqual('Bram');
+  });
 });
