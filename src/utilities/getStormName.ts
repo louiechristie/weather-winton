f4af73ecf97801ca8e54be6400fcde94f49dc92d
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-09 23:42:15 +0000

    feat: don't show storm Bram before start

--- src/utilities/getStormName.ts
+++ src/utilities/getStormName.ts
@@ -1,7 +1,18 @@
 import { Temporal } from 'temporal-polyfill';
 
 const getStormName = (date: Temporal.Instant) => {
-  return 'Bram';
+  const isBefore =
+    date.epochMilliseconds <
+    Temporal.Instant.from('2025-12-08T00+00:00[Europe/London]')
+      .epochMilliseconds;
+  const isAfter =
+    date.epochMilliseconds >
+    Temporal.Instant.from('2025-12-15T00+00:00[Europe/London]')
+      .epochMilliseconds;
+  if (!isBefore && !isAfter) {
+    return 'Bram';
+  }
+  return null;
 };
 
 export default getStormName;
