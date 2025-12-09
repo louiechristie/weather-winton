a3b2df7a61d66423cc9987b4226d60b99bbc5f14
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-09 17:52:12 +0000

    fix: test for storm name Bram

--- src/utilities/getStormName.test.ts
+++ src/utilities/getStormName.test.ts
@@ -5,6 +5,6 @@ describe('get storm name', () => {
   test('should return Bram, when date is 9 December', () => {
     const instant = Temporal.Instant.from('2025-12-09T00:00:00Z');
 
-    expect(getStormName(instant).toEqual('Bram'));
+    expect(getStormName(instant)).toEqual('Bram');
   });
 });
