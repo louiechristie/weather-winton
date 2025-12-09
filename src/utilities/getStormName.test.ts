fa8f972772a489c74b9361544d6bce4cec4f1074
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-09 17:45:41 +0000

    test: storm bram name

---
+++ src/utilities/getStormName.test.ts
@@ -0,0 +1,10 @@
+import { Temporal } from 'temporal-polyfill';
+import getStormName from './getStormName';
+
+describe('get storm name', () => {
+  test('should return Bram, when date is 9 December', () => {
+    const instant = Temporal.Instant.from('2025-12-09T00:00:00Z');
+
+    expect(getStormName(instant).toEqual('Bram'));
+  });
+});
