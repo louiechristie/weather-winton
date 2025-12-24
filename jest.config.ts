b0c394388b5f4b55c89d48ba9992b647694c05b6
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-24 07:22:08 +0000

    test: only show failing unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -108,7 +108,7 @@ const config: JestConfigWithTsJest = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: ['jest-silent-reporter'],
+  reporters: ['jest-silent-reporter'],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
