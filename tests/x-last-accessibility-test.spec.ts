d313422eade62b8d369fce8e42cda7fefbbe8d48
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2026-04-14 09:21:37 +0100

    perf: only run accessibility tests on productoin code for speed of development
    Also because only production setup retries flakey tests in github action

--- tests/x-last-accessibility-test.spec.ts
+++ tests/x-last-accessibility-test.spec.ts
@@ -7,66 +7,68 @@ const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 const stubs = [
   '/',
   '/test/',
-  '/test/fun/',
-  '/test/storm/',
-  '/test/windy/',
   '/test/error/',
+  '/test/fun/',
+  '/test/heatwave/',
+  '/test/windy/',
+  '/test/storm/',
 ];
 
-/* Workaround for flakey test in dev with Next.js message "Latest available version is detected (16.1.1).\">Next.js 16.1.1</span>" */
-if (process.env.NODE_ENV === 'production') stubs.push('/test/heatwave/');
+if (process.env.NODE_ENV === 'production') {
+  test.describe('Accessibility', () => {
+    let totalNumberOfAccessiblityIssues = 0;
 
-test.describe('Accessibility', () => {
-  let totalNumberOfAccessiblityIssues = 0;
+    type Violations = AxeResults['violations']; // the array type
+    type Violation = Violations[number]; // a single violation item
 
-  type Violations = AxeResults['violations']; // the array type
-  type Violation = Violations[number]; // a single violation item
+    interface HashMap<T> {
+      [key: string]: T;
+    }
 
-  interface HashMap<T> {
-    [key: string]: T;
-  }
+    type AccessibilityIssue = Violation & {
+      count: number;
+    };
 
-  type AccessibilityIssue = Violation & {
-    count: number;
-  };
+    // init the map
+    const accessibilityIssueMap: HashMap<AccessibilityIssue> = {};
 
-  // init the map
-  const accessibilityIssueMap: HashMap<AccessibilityIssue> = {};
+    stubs.forEach((stub) => {
+      test(`${stub} should not have any automatically detectable accessibility issues`, async ({
+        page,
+      }) => {
+        await page.goto(`${baseUrl}${stub}`);
 
-  stubs.forEach((stub) => {
-    test(`${stub} should not have any automatically detectable accessibility issues`, async ({
-      page,
-    }) => {
-      await page.goto(`${baseUrl}${stub}`);
+        const accessibilityScanResults = await new AxeBuilder({
+          page,
+        }).analyze();
 
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+        const violations = accessibilityScanResults.violations;
+        const numberOfAccessiblityIssues = violations.length;
 
-      const violations = accessibilityScanResults.violations;
-      const numberOfAccessiblityIssues = violations.length;
+        totalNumberOfAccessiblityIssues =
+          totalNumberOfAccessiblityIssues + numberOfAccessiblityIssues;
 
-      totalNumberOfAccessiblityIssues =
-        totalNumberOfAccessiblityIssues + numberOfAccessiblityIssues;
+        if (numberOfAccessiblityIssues > 0) {
+          violations.forEach((violation) => {
+            accessibilityIssueMap[violation.id] = {
+              ...violation,
+              count: accessibilityIssueMap[violation.id]?.count + 1,
+            };
+            console.warn(violation.description, ': ', violation.nodes.length);
+          });
+        }
 
-      if (numberOfAccessiblityIssues > 0) {
-        violations.forEach((violation) => {
-          accessibilityIssueMap[violation.id] = {
-            ...violation,
-            count: accessibilityIssueMap[violation.id]?.count + 1,
-          };
-          console.warn(violation.description, ': ', violation.nodes.length);
-        });
-      }
-
-      expect(accessibilityScanResults.violations).toEqual([]);
+        expect(accessibilityScanResults.violations).toEqual([]);
+      });
     });
+
+    if (totalNumberOfAccessiblityIssues > 0) {
+      Object.values(accessibilityIssueMap).forEach((issue) => {
+        console.warn(issue.description, ': ', issue.count);
+      });
+    }
+
+    expect(totalNumberOfAccessiblityIssues).toEqual(0);
+    expect(accessibilityIssueMap).toEqual({});
   });
-
-  if (totalNumberOfAccessiblityIssues > 0) {
-    Object.values(accessibilityIssueMap).forEach((issue) => {
-      console.warn(issue.description, ': ', issue.count);
-    });
-  }
-
-  expect(totalNumberOfAccessiblityIssues).toEqual(0);
-  expect(accessibilityIssueMap).toEqual({});
-});
+}
